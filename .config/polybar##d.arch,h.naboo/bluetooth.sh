e725d8ad7bdfb72f8300bb9cb4e272d4d84bdaf3
Umut Dag <umu7d9@gmail.com>
2024-01-16 12:55:42 +0300

    update polybar bluetooth

--- .config/polybar##d.arch,h.naboo/bluetooth.sh
+++ .config/polybar##d.arch,h.naboo/bluetooth.sh
@@ -1,12 +1,38 @@
-#!/bin/sh
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
-then
-  echo "%{F#66ffffff} "
+#!/bin/bash
+blue="%{F#5E81AC}"
+red="%{F#BF616A}"
+
+declare -A battery_map=(
+   ['10']="󰤾"
+   ['20']="󰤿"
+   ['30']="󰥀"
+   ['40']="󰥁"
+   ['50']="󰥂"
+   ['60']="󰥃"
+   ['70']="󰥄"
+   ['80']="󰥅"
+   ['90']="󰥆"
+   ['100']="󰥈"
+)
+
+icon=""
+color=''
+if bluetoothctl show | grep -q "Powered: yes"; then
+    color="$blue"
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then
-    echo ""
-  fi
-  echo "%{F#2193ff} "
+    color="$red"
 fi
+
+icon="${color}${icon}"
+output="$icon"
+dev_mac="$(bluetoothctl info | grep 'Device' | cut -d' ' -f2)"
+if [ "$dev_mac" != '(null)' ]; then
+    percentage="$(bluetoothctl info | grep 'Battery Percentage' | cut -d'(' -f2 | cut -d ')' -f1)"
+    percentage="$(awk -v n="$percentage" 'BEGIN{print int(n / 10) * 10}')"
+    battery="${battery_map[$percentage]}"
+
+    output="$icon $battery $percentage%"
+fi
+
+echo "$output"
 
