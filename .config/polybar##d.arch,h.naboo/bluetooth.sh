e546b77297a19fca2d7abb9dfbd25c5b2a7caa13
Umut Dag <umu7d9@gmail.com>
2024-01-16 10:36:52 +0300

    update polybar bluetooth

--- .config/polybar##d.arch,h.naboo/bluetooth.sh
+++ .config/polybar##d.arch,h.naboo/bluetooth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}"
+  echo "%{F#66ffffff} "
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
+  then
     echo ""
   fi
-  echo "%{F#2193ff}"
+  echo "%{F#2193ff} "
 fi
 
