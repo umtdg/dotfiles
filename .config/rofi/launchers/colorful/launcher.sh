08879c4ad9530526331e42bae53f40a2f538cd2b
Umut Dag <umu7d9@gmail.com>
2024-12-30 01:59:41 +0300

    re-style rofi menus

--- .config/rofi/launchers/colorful/launcher.sh
+++ .config/rofi/launchers/colorful/launcher.sh
@@ -16,9 +16,9 @@ dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
-BG="#2e3440ff"
-FG="#d8dee9ff"
-SELECT="#4c566aff"
+BG="#2E3440FF"
+FG="#D8DEE9FF"
+SELECT="#4C566AFF"
 
 # light
 #ALPHA="#00000000"
@@ -30,7 +30,7 @@ SELECT="#4c566aff"
 COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
 # ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
-ACCENT="#a3be8c"
+ACCENT="#A3BE8CFF"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
