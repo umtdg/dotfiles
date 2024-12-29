89432d363a87f718403eacbfc760750b0cdb28ce
Umut Dag <umu7d9@gmail.com>
2024-12-30 01:56:59 +0300

    switch to i3lock

--- .screenlayout/three.sh
+++ .screenlayout/three.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 3840x2160 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 5760x540 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x540 --rotate normal --output HDMI-A-1-1 --off --output DisplayPort-1-3 --off
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 4480x180 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x180 --rotate normal
