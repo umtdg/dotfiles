089053f65ecb1adcc384f8a0ea058c53232c3553
Umut Dag <umu7d9@gmail.com>
2025-07-16 07:46:44 +0300

    chore: remove unused .screenlayout config

--- .screenlayout/three.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 4480x180 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x180 --rotate normal
