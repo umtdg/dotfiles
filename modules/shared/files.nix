987225d01f3e5962b79d76565f31266a28385529
Umut Dag <umu7d9@gmail.com>
2026-02-17 22:05:41 +0300

    chore: add zsh function incognito to shared config

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -2,5 +2,8 @@
 
 {
   ".config/alacritty/tokyonight_night.toml".source = ./config/alacritty/tokyonight_night.toml;
+
   ".config/tmux/tokyonight-night.conf".source = ./config/tmux/tokyonight-night.conf;
+
+  ".zsh/functions/incognito".source = ./config/zsh/functions/incognito;
 }
