a4720bb9557284e66a06324dc6e774e6d1e6ce0f
Umut Dag <umu7d9@gmail.com>
2026-02-25 10:37:10 +0300

    chore: re-arrange segments
    move nix_shell to the second line
    
    move command_execution_time to the right

--- .p10k.zsh
+++ .p10k.zsh
@@ -52,10 +52,9 @@
     context                   # user@host
     dir                       # current directory
     vcs                       # git status
-    nix_shell                 # nix shell indicator
-    command_execution_time    # previous command duration
     # =========================[ Line #2 ]=========================
     newline                   # \n
+    nix_shell                 # nix shell indicator
     virtualenv                # python virtual environment
     prompt_char               # prompt symbol
   )
@@ -63,7 +62,7 @@
   # Right prompt segments.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    # command_execution_time  # previous command duration
+    command_execution_time  # previous command duration
     # virtualenv              # python virtual environment
     # context                 # user@host
     time                      # current time
